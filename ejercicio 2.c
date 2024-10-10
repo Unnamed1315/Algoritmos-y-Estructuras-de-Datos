8132a05c872086844d7e98817c4724a7cc94be80
Unnamed <141788304+Unnamed1315@users.noreply.github.com>
2024-10-09 23:14:04 -0300

    fix ej. 2

--- ejercicio 2.c
+++ ejercicio 2.c
@@ -9,12 +9,11 @@ int main()  {
     y = -4;
     z = 8;
     b = false;
-    w = true;
+    w = false;
 
     printf("x%%4 == 0: %d \n", x % 4 == 0);
     printf("x+y==0 && y - x == (-1) * z : %d \n", x + y == 0 && y - x == (-1) * z);
     printf("not b %% w : %d \n", !b && w);
     
     return 0;
-
 }
